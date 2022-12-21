6862c1fc13c4f57b27d5e0b6b48c29766b225b23
Rafael Prazeres <rafaelprazeres@ect.ufrn.br>
2022-12-21 16:55:46 -0300

    Semana 3 (continuação)

--- semana_3/test_triangulo.py
+++ semana_3/test_triangulo.py
@@ -3,22 +3,39 @@ import pytest
 
 class TestTriangulo:
 
-    def test_equilatero(self):
-        t = Triangulo.Triangulo(4, 4, 4)
-        assert t.tipo_lado() == "equilátero"
+    @pytest.mark.parametrize("lados, tipo_triangulo", [
+        ([4, 4, 4], "equilátero"),
+        ([3, 4, 5], "escaleno"),
+        ([3, 4, 3], "isósceles"),
+        ([3, 3, 4], "isósceles"),
+        ([4, 3, 3], "isósceles"),
+        ])
 
-    def test_escaleno(self):
-        t = Triangulo.Triangulo(3, 4, 5)
-        assert t.tipo_lado() == "escaleno"
+    def test_tipo_lado(self, lados, tipo_triangulo):
+        t = Triangulo.Triangulo(lados[0], lados[1], lados[2])
+        assert t.tipo_lado() == tipo_triangulo
+        
+    @pytest.mark.parametrize("lados, retangulo", [
+        ([5, 4, 3], True),
+        ([3, 4, 5], True),
+        ([4, 5, 3], True),
+        ([6, 8, 10], True),
+        ([2, 3, 4], False),
+        ])
 
-    def test_isosceles_ac(self):
-        t = Triangulo.Triangulo(3, 4, 3)
-        assert t.tipo_lado() == "isósceles"
+    def test_triangulo_retangulo(self, lados, retangulo):
+        t = Triangulo.Triangulo(lados[0], lados[1], lados[2])
+        assert t.retangulo() == retangulo
 
-    def test_isosceles_ab(self):
-        t = Triangulo.Triangulo(3, 3, 4)
-        assert t.tipo_lado() == "isósceles"
+    @pytest.mark.parametrize("lados_t1, lados_t2, semelhantes", [
+        ([3, 4, 5], [6, 8, 10], True),
+        ([3, 4, 5], [10, 8, 6], True),
+        ([5, 4, 3], [6, 8, 10], True),
+        ([5, 5, 5], [5, 5, 5], True),
+        ([1, 2, 3], [4, 5, 6], False),
+        ])
 
-    def test_isosceles_bc(self):
-        t = Triangulo.Triangulo(4, 3, 3)
-        assert t.tipo_lado() == "isósceles"
+    def test_triangulos_semelhantes(self, lados_t1, lados_t2, semelhantes):
+        t1 = Triangulo.Triangulo(lados_t1[0], lados_t1[1], lados_t1[2])
+        t2 = Triangulo.Triangulo(lados_t2[0], lados_t2[1], lados_t2[2])
+        assert t1.semelhantes(t2) == semelhantes
--- semana_3/triangulo_retangulo.py
+++ semana_3/triangulo_retangulo.py
@@ -24,17 +24,3 @@ class Triangulo:
         if self.c > hipotenusa:
             hipotenusa, cateto_c = self.c, hipotenusa
         return hipotenusa**2 == (cateto_b**2 + cateto_c**2)
-
-    def semelhantes(self, triangulo):
-        t1 = []
-        t2 = []
-        t1.append(self.a), t1.append(self.b), t1.append(self.c), t1.sort()
-        t2.append(triangulo.a), t2.append(triangulo.b), t2.append(triangulo.c), t2.sort()
-        semelhantes = False
-        razao = t1[0] / t2[0]
-        for i in range(1,len(t1)):
-            if t1[i] / t2[i] != razao:
-                break
-            else:
-                semelhantes = True
-        return semelhantes
